148ce2490bc4a756d5ea44b2d9021808d51b91bc
Linda <linda@hellomynameislinda.com>
2024-04-04 17:57:00 +0200

    Todo redone and finished.

--- todo.js
+++ todo.js
@@ -1,31 +1,34 @@
-const form = document.getElementById('add-new-form');
+const form = document.querySelector('#add-new-form');
 form.addEventListener('submit', function (event) {
     event.preventDefault(); // To make sure we don't submit
 
-    const output = document.getElementById('todo-list');
+    const output = document.querySelector('#todo-list');
 
     const newItem = form.elements['add-new-item'].value;
-    form.elements['add-new-item'].value = "";
+    event.target.reset();
+
+    document.querySelector('#new-list-message').className = "invisible";
+
     let li = document.createElement('li');
-    li.innerHTML = `<input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckboxStretched"><label class="form-check-label stretched-link" for="thirdCheckboxStretched">${newItem}</label>`;
-    li.className = 'list-group-item';
+    li.className = 'list-group-item d-flex bd-highlight';
 
+    let div = document.createElement('div');
+    div.className = "flex-grow-1";
+    div.id ="list-item-1"
+    div.innerHTML = newItem;
+    div.addEventListener('click', (e) => {
+        e.target.classList.toggle('text-decoration-line-through')
+    })
+    li.appendChild(div);
 
+    let button = document.createElement('button');
+    button.className = "btn";
+    button.id ="list-button-1"
+    button.innerHTML = '<i class="bi bi-trash3"></i>';
+    button.addEventListener('click', (e) => {
+        e.currentTarget.parentNode.remove();
+    })
+    li.appendChild(button);
 
-
-    /*    for (let index = fromNum; index <= toNum; index++) {
-            if ((index % bishNum == 0) && (index % boshNum == 0))
-                outputString += 'Bish-Bosh';
-            else if (index % bishNum == 0)
-                outputString += 'Bish';
-            else if (index % boshNum == 0)
-                outputString += 'Bosh';
-            else
-                outputString += index;
-    
-            if (index < toNum)
-                outputString += ', '
-        }
-        */
     output.appendChild(li);
 });
